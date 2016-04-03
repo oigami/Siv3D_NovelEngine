09d2ea5d77adceffa8b4f589cd8c63dbf7a935bd
oigami <oigami013@yahoo.co.jp>
2016-04-04 01:03:16 +0900

    仕様を少し変更

--- mmd_novel/MmdNovel/default_value.h
+++ mmd_novel/MmdNovel/default_value.h
@@ -2,19 +2,14 @@
 namespace kag {
   namespace detail {
     struct Default {
-      constexpr operator int() const { return -1; }
+      constexpr operator int() const { return std::numeric_limits<int>::min() + 10000000; }
+      template<class Type> constexpr bool operator==(const Type& t) const {
+        return t == static_cast<Type>(Define::default);
+      }
     };
     template<class Type> constexpr bool operator==(const Type& t, const Default&) {
-      return t == static_cast<Type>(Define::default);
+      return Define::default == t;
     }
-  }
-  /// <summary>
-  /// kagで使用する各種定数値
-  /// </summary>
-  namespace Define {
-    constexpr int fore_page = 0;
-    constexpr int back_page = 1;
-    constexpr detail::Default default;
   }
 
   template<class Type> class Value {
@@ -34,10 +29,29 @@ namespace kag {
     /// </summary>
     /// <param name="def"></param>
     /// <returns></returns>
-    Type operator()(Type def) const { return t == Define::default ? def : t; }
+    constexpr Type operator()(Type def) const { return t == Define::default ? def : t; }
   private:
 
     Type t;
   };
+
+  /// <summary>
+  /// kagで使用する各種定数値
+  /// </summary>
+  namespace Define {
+
+    // 背景レイヤのインデックス値
+    // 前景レイヤは1から始める
+    constexpr int base_image = 0;
+
+    // 表ページ
+    constexpr int fore_page = 0;
+
+    // 裏ページ
+    constexpr int back_page = 1;
+
+    //省略した時の値
+    constexpr kag::detail::Default default;
+  }
 
 }
