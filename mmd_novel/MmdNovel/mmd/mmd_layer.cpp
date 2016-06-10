8ae10d58e3222a9438eaefd3238147ac1c94584d
oigami <oigami013@yahoo.co.jp>
2016-06-10 21:18:51 +0900

    入れ忘れてたファイルの追加

---
+++ mmd_novel/MmdNovel/mmd/mmd_layer.cpp
@@ -0,0 +1,25 @@
+#include "mmd_layer.h"
+namespace kag {
+  namespace pimpl {
+
+    void MMDLayerPimpl::SetModel(s3d_mmd::MMD model) {
+      model_ = model;
+    }
+    void MMDLayerPimpl::SetVMD(s3d_mmd::VMD vmd) {
+      vmd_ = vmd;
+      vmd_.setTime(0);
+      vmd_.IsLoop(true, 0);
+    }
+    void MMDLayerPimpl::update() {
+      if (model_.isOpen()) {
+        vmd_.UpdateTime();
+        vmd_.UpdateBone(*model_.bones());
+      }
+    }
+    void MMDLayerPimpl::draw() const {
+      if (model_.isOpen()) {
+        model_.draw(vmd_);
+      }
+    }
+  }
+}
---
+++ mmd_novel/MmdNovel/mmd/mmd_layer.h
@@ -0,0 +1,31 @@
+#pragma once
+#include <MmdNovel/layer.h>
+#include <MMD.h>
+namespace kag {
+  namespace pimpl {
+    class MMDLayerPimpl : public  Layer {
+      friend LayerHelper<MMDLayerPimpl>;
+    public:
+      MMDLayerPimpl() = default;
+      static std::shared_ptr<MMDLayerPimpl> create() {
+        return std::make_shared<MMDLayerPimpl>();
+      }
+
+      void SetModel(s3d_mmd::MMD model);
+
+      void SetVMD(s3d_mmd::VMD vmd);
+
+      void update();
+
+      void draw() const;
+
+    private:
+
+      s3d_mmd::VMD vmd_;
+      s3d_mmd::MMD model_;
+
+    };
+  }
+
+  using MMDLayer = LayerHelper<pimpl::MMDLayerPimpl>;
+}
